cd4193a67dbe4eadc722f6e7cb32c03454c00f4e
marcelocruzsilverio <marcelosilveriocruz@gmail.com>
2024-10-14 15:39:23 -0300

    Criando a consulta SQL no MovieRepository e testando

--- src/main/java/com/devsuperior/uri2611/Uri2611Application.java
+++ src/main/java/com/devsuperior/uri2611/Uri2611Application.java
@@ -1,12 +1,36 @@
 package com.devsuperior.uri2611;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.devsuperior.uri2611.projections.MovieMinProjection;
+import com.devsuperior.uri2611.repositories.MovieRepository;
+
 @SpringBootApplication
-public class Uri2611Application {
+public class Uri2611Application implements CommandLineRunner {
+	
+	@Autowired
+	private MovieRepository repository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Uri2611Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		List<MovieMinProjection> list = repository.serach1("Action");
+//		List<CustomerMinDTO> result1 = list.stream().map(x -> new CustomerMinDTO(x)).collect(Collectors.toList());
+		
+		System.out.println("\n*** RESULTADO SQL RAIZ");
+		for (MovieMinProjection obj : list) {
+			System.out.println(obj.getName());
+			System.out.println(obj.getId());
+		}
+		
 	}
 }
